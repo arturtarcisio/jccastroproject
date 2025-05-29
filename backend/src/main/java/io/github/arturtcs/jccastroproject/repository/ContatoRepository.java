ed6b385cdd7fd6ef3ef2de140068d56059d92bf4
arturtarcisio <arturtarcisio1@gmail.com>
2025-05-29 18:13:23 -0300

    Criando consulta para retornar contato pelo celular

--- backend/src/main/java/io/github/arturtcs/jccastroproject/repository/ContatoRepository.java
+++ backend/src/main/java/io/github/arturtcs/jccastroproject/repository/ContatoRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ContatoRepository extends JpaRepository<Contato, Long> {
+
+    Contato findByCelular(String celular);
+
 }
