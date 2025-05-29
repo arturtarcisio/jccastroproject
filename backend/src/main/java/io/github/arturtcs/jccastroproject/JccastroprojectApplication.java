76c7ed1790eb3530f39a1652c963390d1feb01fb
arturtarcisio <arturtarcisio1@gmail.com>
2025-05-29 17:56:14 -0300

    Projeto criado

---
+++ backend/src/main/java/io/github/arturtcs/jccastroproject/JccastroprojectApplication.java
@@ -0,0 +1,13 @@
+package io.github.arturtcs.jccastroproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JccastroprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JccastroprojectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/github/arturtcs/jccastroproject/JccastroprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.arturtcs.jccastroproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JccastroprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
