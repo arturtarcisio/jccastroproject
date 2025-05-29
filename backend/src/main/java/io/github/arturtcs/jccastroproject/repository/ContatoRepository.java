a1da22999b3f080a95d5a27b5b6944642b57b059
arturtarcisio <arturtarcisio1@gmail.com>
2025-05-29 18:11:40 -0300

    Criando repository de Contato

---
+++ backend/src/main/java/io/github/arturtcs/jccastroproject/repository/ContatoRepository.java
@@ -0,0 +1,9 @@
+package io.github.arturtcs.jccastroproject.repository;
+
+import io.github.arturtcs.jccastroproject.domain.Contato;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContatoRepository extends JpaRepository<Contato, Long> {
+}
