84f95d1ffc80ae390fb08a4a0f88815e3fbd9c9e
arturtarcisio <arturtarcisio1@gmail.com>
2025-05-29 22:50:50 -0300

    Criando guards e os componentes

---
+++ contato-frontend/src/app/components/contato-form/contato-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contato-form',
+  imports: [],
+  templateUrl: './contato-form.html',
+  styleUrl: './contato-form.scss'
+})
+export class ContatoForm {
+
+}
---
+++ contato-frontend/src/app/components/contato-list/contato-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contato-list',
+  imports: [],
+  templateUrl: './contato-list.html',
+  styleUrl: './contato-list.scss'
+})
+export class ContatoList {
+
+}
