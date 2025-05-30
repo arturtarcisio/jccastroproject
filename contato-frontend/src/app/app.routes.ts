4bb78a292847c7a53b7ddad02e12f8b4a073da3c
arturtarcisio <arturtarcisio1@gmail.com>
2025-05-29 22:59:21 -0300

    Configurando rotas e config

--- contato-frontend/src/app/app.routes.ts
+++ contato-frontend/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { ContatoList } from './components/contato-list/contato-list';
+import { ContatoForm } from './components/contato-form/contato-form';
+import { AuthGuard } from './guards/auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { 
+    path: 'contatos', 
+    component: ContatoList,
+    canActivate: [AuthGuard] 
+  },
+  { 
+    path: 'contatos/editar/:id', 
+    component: ContatoForm,
+    canActivate: [AuthGuard] 
+  },
+  { path: '**', redirectTo: '' }
+];
